c76ad6fca7d6d93f9be612a4a186696d632e7a6d
Henry Pan <henry-pan@users.noreply.github.com>
2016-03-11 18:29:35 -0800

    Temporary listview

--- AnimeAlert/app/src/main/java/com/finalapp/teamhls/animealert/UserChartActivity.java
+++ AnimeAlert/app/src/main/java/com/finalapp/teamhls/animealert/UserChartActivity.java
@@ -1,10 +1,16 @@
 package com.finalapp.teamhls.animealert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class UserChartActivity extends AppCompatActivity implements View.OnClickListener {
     public static String LOG_TAG = "My Log Tag";
@@ -15,8 +21,32 @@ public class UserChartActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_user_chart);
         Log.i(LOG_TAG, "In UserCharts");
 
-        Button optButton = (Button) findViewById(R.id.OptButton);
-        optButton.setOnClickListener(this);
+        ListView chartList = (ListView) findViewById(R.id.CurrentChartList);
+
+
+        //Temporary
+        String[] anime = new String [] {"404", "Cowboy Bebop", "404", "404", "404", "Cowboy Bebop: Tengoku no Tobira","Trigun",
+                "Witch Hunter Robin","Beet the Vandel Buster","404", "404", "404", "404",
+                "404","404","Eyeshield 21","Hachimitsu to Clover","Hungry Heart: Wild Striker"};
+        final ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < anime.length; ++i) {
+            list.add(anime[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        chartList.setAdapter(adapter);
+
+        chartList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                String mal_id = Long.toString(id);
+                Intent testAnime = new Intent(UserChartActivity.this, ViewAnimeActivity.class);
+                testAnime.putExtra("malNum",mal_id);
+                UserChartActivity.this.startActivity(testAnime);
+            }
+        });
+
+
     }
 
     @Override
@@ -27,4 +57,6 @@ public class UserChartActivity extends AppCompatActivity implements View.OnClick
             finish();
         }
     }
+
+
 }
