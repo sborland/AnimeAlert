94055d49498a91909e40716ec93cb26ba25ffc72
Hui Shi Li <huishili95@gmail.com>
2016-03-11 18:43:35 -0800

    added user database

---
+++ AnimeAlert/app/src/main/java/com/finalapp/teamhls/animealert/classes/DBUser.java
@@ -0,0 +1,50 @@
+package com.finalapp.teamhls.animealert.classes;
+
+/**
+ * Created by HuiShi on 3/11/2016.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBUser  extends SQLiteOpenHelper {
+    //version number to upgrade database version
+    //each time if you Add, Edit table, you need to change the
+    //version number.
+
+    //database name
+    private static final String DATABASE_NAME = "userChart.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBUser(Context context ) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //All necessary tables you like to create will create here
+
+        String CREATE_TABLE_ANIMECHART = "CREATE TABLE " + AnimeChart.TABLE  + "("
+                + AnimeChart.KEY_malNum  + " INTEGER ,"
+                + AnimeChart.KEY_title + " TEXT, "
+                + AnimeChart.KEY_airDate + " INTEGER, "
+                + AnimeChart.KEY_simulCast + " TEXT, "
+                + AnimeChart.KEY_isShort + " TEXT, "
+                + AnimeChart.KEY_currEp + " INTEGER )";
+
+        db.execSQL(CREATE_TABLE_ANIMECHART);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed, all data will be gone!!!
+        db.execSQL("DROP TABLE IF EXISTS " + AnimeChart.TABLE);
+
+        // Create tables again
+        onCreate(db);
+
+    }
+
+}
---
+++ AnimeAlert/app/src/main/java/com/finalapp/teamhls/animealert/classes/UserDB.java
@@ -0,0 +1,154 @@
+package com.finalapp.teamhls.animealert.classes;
+
+/**
+ * Created by HuiShi on 3/11/2016.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Created by HuiShi on 3/6/2016.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserDB {
+    private DBUser dbUser;
+    public static String LOG_TAG = "My Log Tag";
+
+    public UserDB(Context context) {
+        dbUser = new DBUser(context);
+    }
+
+    public void insert(AnimeChart chart) {
+
+        //Open connection to write data
+        SQLiteDatabase db = dbUser.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(AnimeChart.KEY_malNum, chart.malNum);
+        values.put(AnimeChart.KEY_title,chart.title);
+        values.put(AnimeChart.KEY_airDate, chart.airDate);
+        values.put(AnimeChart.KEY_simulCast, chart.simulCast);
+        values.put(AnimeChart.KEY_isShort, chart.isShort);
+        values.put(AnimeChart.KEY_currEp, chart.currEp);
+
+
+        // Inserting Row
+        db.insert(AnimeChart.TABLE, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public void delete(int malNum) {
+
+        SQLiteDatabase db = dbUser.getWritableDatabase();
+        // It's a good practice to use parameter ?, instead of concatenate string
+        db.delete(AnimeChart.TABLE, AnimeChart.KEY_malNum + "= ?", new String[] { String.valueOf(malNum) });
+        db.close(); // Closing database connection
+    }
+
+    public void update(AnimeChart chart) {
+
+        SQLiteDatabase db = dbUser.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(AnimeChart.KEY_malNum, chart.malNum);
+        values.put(AnimeChart.KEY_title,chart.title);
+        values.put(AnimeChart.KEY_airDate, chart.airDate);
+        values.put(AnimeChart.KEY_simulCast, chart.simulCast);
+        values.put(AnimeChart.KEY_isShort, chart.isShort);
+        values.put(AnimeChart.KEY_currEp, chart.currEp);
+
+
+        // It's a good practice to use parameter ?, instead of concatenate string
+        db.update(AnimeChart.TABLE, values, AnimeChart.KEY_malNum + "= ?", new String[] { String.valueOf(chart.malNum) });
+        db.close(); // Closing database connection
+    }
+
+    public ArrayList<HashMap<String, String>> getAnimeChart() {
+        //Open connection to read only
+        SQLiteDatabase db = dbUser.getReadableDatabase();
+        String selectQuery =  "SELECT  " +
+                AnimeChart.KEY_title + "," +
+                AnimeChart.KEY_malNum + "," +
+                AnimeChart.KEY_airDate + "," +
+                AnimeChart.KEY_simulCast + "," +
+                AnimeChart.KEY_isShort + "," +
+                AnimeChart.KEY_currEp +
+                " FROM " + AnimeChart.TABLE;
+        ArrayList<HashMap<String, String>> animeList = new ArrayList<HashMap<String, String>>();
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> anime = new HashMap<String, String>();
+                anime.put("title", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_title)));
+                anime.put("malNum", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_malNum)));
+                anime.put("airDate", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_airDate)));
+                anime.put("simulCast", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_simulCast)));
+                anime.put("isShort", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_isShort)));
+                anime.put("currEp", cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_currEp)));
+                animeList.add(anime);
+
+            } while (cursor.moveToNext());
+        }
+        Log.i(LOG_TAG, animeList.size() + "");
+
+        cursor.close();
+        db.close();
+
+        return animeList;
+
+    }
+
+    public AnimeChart getAnimeByMalNum(int malNum){
+        SQLiteDatabase db = dbUser.getReadableDatabase();
+        String selectQuery =  "SELECT  " +
+                AnimeChart.KEY_title + "," +
+                AnimeChart.KEY_malNum + "," +
+                AnimeChart.KEY_airDate + "," +
+                AnimeChart.KEY_simulCast + "," +
+                AnimeChart.KEY_isShort + "," +
+                AnimeChart.KEY_currEp +
+                " FROM " + AnimeChart.TABLE
+                + " WHERE " +
+                AnimeChart.KEY_malNum + "=?";// It's a good practice to use parameter ?, instead of concatenate string
+
+        int iCount =0;
+        AnimeChart chart = new AnimeChart();
+
+        Cursor cursor = db.rawQuery(selectQuery, new String[] { String.valueOf(malNum) } );
+
+        if (cursor.moveToFirst()) {
+            do {
+                chart.title =cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_title));
+                chart.malNum =cursor.getInt(cursor.getColumnIndex(AnimeChart.KEY_malNum));
+                chart.airDate  =cursor.getLong(cursor.getColumnIndex(AnimeChart.KEY_airDate));
+                chart.simulCast =cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_simulCast));
+                chart.isShort  =cursor.getString(cursor.getColumnIndex(AnimeChart.KEY_isShort));
+                chart.currEp =cursor.getInt(cursor.getColumnIndex(AnimeChart.KEY_currEp));
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return chart;
+    }
+
+}
